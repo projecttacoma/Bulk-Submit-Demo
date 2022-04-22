50d851e1312e169aef4c377d613f7cd8d9787b2e
Chris Hossenlopp <hossenlopp@mitre.org>
2022-04-22 10:08:33 -0400

    Added input change test to ExportURLInput

--- __tests__/components/ExportURLInput.test.tsx
+++ __tests__/components/ExportURLInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent as fhirEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { mantineWrap } from '../helpers/testHelpers';
 import ExportURLInput from '../../components/ExportURLInput';
@@ -7,7 +7,10 @@ describe('ExportURLInput', () => {
   it('renders a text input field', () => {
     render(mantineWrap(<ExportURLInput />));
 
-    const textbox = screen.getByPlaceholderText('Export URL (Data Source)');
+    const textbox = screen.getByPlaceholderText('Export URL (Data Source)') as HTMLInputElement;
     expect(textbox).toBeInTheDocument();
+
+    fhirEvent.change(textbox, { target: { value: 'http://localhost:3001/' } });
+    expect(textbox.value).toBe('http://localhost:3001/');
   });
 });
