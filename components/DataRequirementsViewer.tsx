8ceb1ddb340bcf52d922e69b6f6fbf667b9dfe9f
natjoe4 <njjones@mitre.org>
2022-04-22 14:42:33 -0400

    Updated testing and reworked function structure

--- components/DataRequirementsViewer.tsx
+++ components/DataRequirementsViewer.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { selectedMeasureState } from '../atoms/selectedMeasure';
-import { Loader, ScrollArea, Card, Tabs, Title, Text } from '@mantine/core';
+import { Loader, ScrollArea, Card, Center, Tabs, Title, Text } from '@mantine/core';
 import dynamic from 'next/dynamic';
 const DynamicReactJson = dynamic(import('react-json-view'), { ssr: false });
 
 export default function DataRequirementsViewer() {
   const [selectedMeasure] = useRecoilState(selectedMeasureState);
   const [dataRequirements, setDataRequirements] = useState<fhir4.DataRequirement | null>(null);
+  const [error, setError] = useState<fhir4.OperationOutcome | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const dataRequirementsQuery = '?periodStart=2019-01-01&periodEnd=2019-12-31';
   useEffect(() => {
@@ -18,9 +19,13 @@ export default function DataRequirementsViewer() {
         .then(res => res.json())
         .then(dr => {
           setDataRequirements(dr);
+          setError(null);
           setIsLoading(false);
         })
-        .catch(console.log);
+        .catch(e => {
+          setError(e);
+          setIsLoading(false);
+        });
     }
   }, [selectedMeasure]);
 
@@ -29,18 +34,38 @@ export default function DataRequirementsViewer() {
       <Title order={4}>Data Requirements</Title>
       <Tabs>
         <Tabs.Tab label="JSON">
-          <ScrollArea style={{ height: 300 }}>
-            {isLoading && <Loader />}
-            {!isLoading && dataRequirements && (
-              <DynamicReactJson src={dataRequirements} indentWidth={2} collapsed={true} />
-            )}
-            {!isLoading && !dataRequirements && <Text>No Data</Text>}
-          </ScrollArea>
+          <ScrollArea style={{ height: 300 }}>{renderJSONResults(isLoading, dataRequirements, error)}</ScrollArea>
         </Tabs.Tab>
         <Tabs.Tab label="Filters">
-          <Text>Filter preview coming soon to a Bulk Submit Demo near you!</Text>
+          <ScrollArea style={{ height: 300 }}>
+            <Text>Filter preview coming soon to a Bulk Submit Demo near you!</Text>
+          </ScrollArea>
         </Tabs.Tab>
       </Tabs>
     </Card>
   );
+}
+
+function renderJSONResults(
+  isLoading: boolean,
+  dataRequirements: fhir4.DataRequirement | null,
+  error: fhir4.OperationOutcome | null
+) {
+  if (isLoading) {
+    return (
+      <Center>
+        <Loader />
+      </Center>
+    );
+  } else if (error) {
+    return <DynamicReactJson src={error} indentWidth={2} />;
+  } else if (dataRequirements) {
+    return <DynamicReactJson src={dataRequirements} indentWidth={2} />;
+  } else {
+    return (
+      <Center>
+        <Text>No Data</Text>
+      </Center>
+    );
+  }
 }
