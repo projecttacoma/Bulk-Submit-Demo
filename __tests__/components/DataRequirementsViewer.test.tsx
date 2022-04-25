d79081bb8ab47990852d713b43b82576dfb2ab33
natjoe4 <njjones@mitre.org>
2022-04-25 16:27:48 -0400

    expanded testing

--- __tests__/components/DataRequirementsViewer.test.tsx
+++ __tests__/components/DataRequirementsViewer.test.tsx
@@ -3,17 +3,26 @@ import '@testing-library/jest-dom';
 import { mantineRecoilWrap } from '../helpers/testHelpers';
 import DataRequirementsViewer from '../../components/DataRequirementsViewer';
 
-describe('ExportURLInput', () => {
-  it('renders a text input field', () => {
-    window.ResizeObserver = jest.fn().mockImplementation(() => ({
-      observe: jest.fn(),
-      unobserve: jest.fn(),
-      disconnect: jest.fn()
-    }));
-
+describe('DataRequirementsViewer', () => {
+  // Workaround for issues with the built-in use-resize-observer in jest
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn()
+  }));
+  it('renders a JSON tab', () => {
     render(mantineRecoilWrap(<DataRequirementsViewer />));
-
-    const JSONTab = screen.getByRole('tab', { name: 'nonsense' }) as HTMLInputElement;
+    const JSONTab = screen.getByRole('tab', { name: 'JSON' }) as HTMLInputElement;
     expect(JSONTab).toBeInTheDocument();
+  });
+  it('renders a Filters tab', () => {
+    render(mantineRecoilWrap(<DataRequirementsViewer />));
+    const FilterTab = screen.getByRole('tab', { name: 'Filters' }) as HTMLInputElement;
+    expect(FilterTab).toBeInTheDocument();
+  });
+  it('renders content inside JSON tab', () => {
+    render(mantineRecoilWrap(<DataRequirementsViewer />));
+    const JSONViewer = screen.getByText('No Data') as HTMLInputElement;
+    expect(JSONViewer).toBeInTheDocument();
   });
 });
