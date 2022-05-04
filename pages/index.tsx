320899d2c55465980b243b2cf4ea85cb95a7aa0c
sarahmcdougall <smcdougall@mitre.org>
2022-05-04 15:15:41 -0400

    change button location

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { AppShell, Header, Grid } from '@mantine/core';
+import { AppShell, Header, Grid, Space } from '@mantine/core';
 import AbacusHeader from '../components/AbacusHeader';
 import MeasureSelect from '../components/MeasureSelect';
 import ExportURLInput from '../components/ExportURLInput';
@@ -14,6 +14,10 @@ const Home: NextPage = () => {
         <title>Bulk Submit Data Demo Application</title>
       </Head>
       <AppShell padding="md" header={<Header height={60}>{<AbacusHeader></AbacusHeader>}</Header>}>
+        <Grid justify="center">
+          <ResetInputsButton />
+        </Grid>
+        <Space h="lg" />
         <Grid>
           <Grid.Col span={6}>
             <MeasureSelect />
@@ -22,7 +26,6 @@ const Home: NextPage = () => {
             <ExportURLInput />
           </Grid.Col>
           <Grid.Col span={6}>
-            <ResetInputsButton />
             <DataRequirementsPanel />
           </Grid.Col>
         </Grid>
